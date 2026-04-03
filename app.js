1a515ecbec2f0903eb57736d93504ffd2457c4d6
ayush4742 <ayushkumarq43@gmail.com>
2026-04-04 01:10:05 +0530

    Improve Swagger UI

--- app.js
+++ app.js
@@ -10,21 +10,37 @@ const { swaggerUi, specs } = require("./config/swagger");
 
 const app = express();
 
+// Connect DB
 connectDB();
 
+// Middleware
 app.use(express.json());
 
-// Swagger
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+// Swagger UI
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, {
+    explorer: true,
+    swaggerOptions: {
+      docExpansion: "list",
+      filter: true,
+      defaultModelsExpandDepth: -1,
+    },
+  })
+);
 
+// Routes
 app.use("/api/users", userRoutes);
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 
+// Home Route
 app.get("/", (req, res) => {
   res.send("Finance Dashboard Backend Running");
 });
 
+// Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
--- config/swagger.js
+++ config/swagger.js
@@ -5,15 +5,56 @@ const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "Finance Dashboard API",
+      title: "Finance Dashboard Backend API",
       version: "1.0.0",
-      description: "Finance Dashboard Backend APIs",
+      description: `
+# Finance Dashboard Backend API
+
+This API provides:
+
+## Features
+- User Authentication (JWT)
+- Role Based Access Control
+- Transactions Management
+- Dashboard Analytics
+
+## Roles
+
+- Admin → Full access  
+- Analyst → Create & View  
+- Viewer → Read only  
+
+## How To Test
+
+1. Create demo users → **POST /api/users/seed**
+2. Login → **POST /api/users/login**
+3. Copy token
+4. Click **Authorize**
+5. Test APIs
+      `,
     },
+
     servers: [
       {
         url: "https://finance-dashboard-backend-wu1m.onrender.com",
       },
     ],
+
+    tags: [
+      {
+        name: "Users",
+        description: "Authentication & User Management",
+      },
+      {
+        name: "Transactions",
+        description: "Financial Transactions APIs",
+      },
+      {
+        name: "Dashboard",
+        description: "Dashboard Analytics APIs",
+      },
+    ],
+
     components: {
       securitySchemes: {
         bearerAuth: {
@@ -23,12 +64,14 @@ const options = {
         },
       },
     },
+
     security: [
       {
         bearerAuth: [],
       },
     ],
   },
+
   apis: ["./routes/*.js"],
 };
 
